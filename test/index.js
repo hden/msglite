8a9f9d0e531bd13601b6d7525e5b6092e3bb50a8
Hao-kang Den <haokang.den@gmail.com>
2015-07-21 21:16:14 +0800

    validates again schema

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
+import joi from 'joi'
 import sqlite from 'sqlite3'
 import { expect } from 'chai'
 import createAccessor from '../src/index'
+
 describe('accessor', () => {
   let db, foobar
 
@@ -28,6 +30,31 @@ describe('accessor', () => {
       .then(foobar.get)
       .then((obj) => {
         expect(obj).to.deep.equal(fixture)
+      })
+  })
+
+  it('validate write', () => {
+    const fixture = { value: 1 }
+    const schema = joi.object().keys({ value: joi.string() })
+
+    return foobar
+      .validate('write', schema)
+      .insert(fixture)
+      .catch((error) => {
+        expect(error).to.have.property('message', 'child "value" fails because ["value" must be a string]')
+      })
+  })
+
+  it('validate read', () => {
+    const fixture = { value: 1 }
+    const schema = joi.object().keys({ value: joi.string() })
+
+    return foobar
+      .validate('read', schema)
+      .insert(fixture)
+      .then(foobar.get)
+      .catch((error) => {
+        expect(error).to.have.property('message', 'child "value" fails because ["value" must be a string]')
       })
   })
 })
